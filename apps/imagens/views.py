7be9857bf0b5f99739064c100ecb93ee70861840
Andre Emidio <andre@datatype.com.br>
2020-06-14 01:30:22 -0300

    Corrigindo

--- apps/imagens/views.py
+++ apps/imagens/views.py
@@ -10,18 +10,16 @@ from django.views.decorators import gzip
 
 class VideoCamera(object):
     def __init__(self):
-        URL = 'apps/utils/video/WIN_20200611_22_01_47_Pro.mp4'
-        # URL = 'apps/utils/video/ezgif.com-gif-makerx8.mp4'
+        # URL = 'apps/utils/video/WIN_20200611_22_01_47_Pro.mp4'
+        URL = 'apps/utils/video/ezgif.com-gif-makerx8.mp4'
         self.video = cv2.VideoCapture(URL)
         # self.video = cv2.VideoCapture(1)
-        # self.video = acapture.open(1,frame_capture=True)
-        # self.video = acapture.open(URL,frame_capture=True)
+
 
     def __del__(self):
         self.video.release()
 
     def get_frame(self):
-        # HAARCASCADE_FACE = 'apps/utils/haarcascade/haarcascade_frontalcatface.xml'
         ret,image = self.video.read()
         image = cv2.resize(image, None, fx=0.2, fy=0.2)
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
