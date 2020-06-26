16bd076c247342cb15770f755d54072b07183b3b
Andre Emidio <andre@datatype.com.br>
2020-06-25 23:16:26 -0300

    Correção da rota de processamento de imagens e implantação do DLIB

--- apps/imagens/urls.py
+++ apps/imagens/urls.py
@@ -1,13 +1,18 @@
+from django.conf.urls import url
 from django.urls import path
 from . import views
 from django.urls.conf import include
 from django.urls import path
 from django.contrib.auth.views import LoginView
 
+from .api.viewsets import get_images, ocr_image
 from .views import index
 
 urlpatterns = [
 
     path('', index, name='index'),
+
+    url(r'^imagens-tests', get_images, name='get_delete_update_puppy'),
+    url(r'^imagens-ocr', ocr_image, name='ocr_image'),
 
 ]
